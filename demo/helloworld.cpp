9e133a63def952b397cf831f0f4edf4aa3261700
Michael Campbell <me@micycle.dev>
2025-08-29 02:46:01 -0400

    add more benches

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <sol/forward.hpp>
 #include <vector>
 
 // #define SOL_ALL_SAFETIES_ON 1
@@ -67,7 +68,7 @@ void repeat(const Func func, int times) {
         func();
 }
 
-void cpp_bench() {
+void native_bench(sol::state& lua) {
     std::vector<Vec2> vs;
     for (int idx = 0; idx < NUM_VECS; idx++) {
         vs.push_back(Vec2((float)rand() / RAND_MAX, (float)rand() / RAND_MAX));
@@ -82,16 +83,47 @@ void cpp_bench() {
         sum += v.y;
     };
 
-    std::cout << "cpp_bench: ";
+    std::cout << "native: ";
     bench([&](){
         for (Vec2& v : vs) {
             translate(v);
         }
     });
-    std::cout << sum << std::endl;
+    lua["sum"] = lua["sum"].get<float>() + sum;
 }
 
-void ptr_bench(sol::state& lua) {
+void cpp_func_lua_mem(sol::state& lua) {
+    std::vector<sol::table> vs;
+    for (int idx = 0; idx < NUM_VECS; idx++) {
+        float x = (float)rand() / RAND_MAX;
+        float y = (float)rand() / RAND_MAX;
+
+        auto v = lua.create_table();
+        v["x"] = x;
+        v["y"] = y;
+
+        vs.push_back(v);
+    }
+
+    float sum = 0.f;
+    auto translate = [&](sol::table& v) {
+        v["x"] = v["x"].get<float>() + 2;
+        v["y"] = v["y"].get<float>() + 2;
+
+        sum += v["x"].get<float>();
+        sum += v["y"].get<float>();
+    };
+
+    std::cout << "cpp_func_lua_mem: ";
+    bench([&](){
+        for (sol::table& v : vs) {
+            translate(v);
+        }
+    });
+    lua["sum"] = lua["sum"].get<float>() + sum;
+}
+
+void cpp_mem_lua_func(sol::state& lua) {
     std::vector<Vec2> vs;
     for (int idx = 0; idx < NUM_VECS; idx++) {
         vs.push_back(Vec2((float)rand() / RAND_MAX, (float)rand() / RAND_MAX));
@@ -99,7 +131,7 @@ void ptr_bench(sol::state& lua) {
 
     sol::function translate = lua["translate"];
 
-    std::cout << "ptr_bench: ";
+    std::cout << "cpp_mem_lua_func: ";
     bench([&](){
         for (Vec2& v : vs) {
             translate(&v);
@@ -107,7 +139,53 @@ void ptr_bench(sol::state& lua) {
     });
 }
 
-void table_bench(sol::state& lua) {
+void cpp_mem_lua_func_aot(sol::state& lua) {
+    std::vector<Vec2> vs;
+    for (int idx = 0; idx < NUM_VECS; idx++) {
+        vs.push_back(Vec2((float)rand() / RAND_MAX, (float)rand() / RAND_MAX));
+    }
+
+    std::vector<sol::object> lua_ready;
+    for (int idx = 0; idx < NUM_VECS; idx++) {
+        auto& v = vs[idx];
+        lua_ready.push_back(sol::make_object(lua, &v));
+    }
+
+    sol::function translate = lua["translate"];
+
+    std::cout << "cpp_mem_lua_func_aot: ";
+    bench([&](){
+        for (sol::object& v : lua_ready) {
+            translate(v);
+        }
+    });
+}
+
+void cpp_glue_lua_all(sol::state& lua) {
+    std::vector<sol::table> vs;
+    sol::function translate = lua["translate"];
+    vs.reserve(NUM_VECS); // we're not measuring the time to expand the vector
+
+    std::cout << "cpp_glue_lua_all: ";
+    bench([&](){
+        for (int idx = 0; idx < NUM_VECS; idx++) {
+            float x = (float)rand() / RAND_MAX;
+            float y = (float)rand() / RAND_MAX;
+
+            auto v = lua.create_table();
+            v["x"] = x;
+            v["y"] = y;
+
+            vs.push_back(v);
+        }
+
+        for (sol::table& v : vs) {
+            translate(v);
+        }
+    });
+}
+
+void cpp_glue_lua_all_aot(sol::state& lua) {
     std::vector<sol::table> vs;
     for (int idx = 0; idx < NUM_VECS; idx++) {
         float x = (float)rand() / RAND_MAX;
@@ -122,7 +200,7 @@ void table_bench(sol::state& lua) {
 
     sol::function translate = lua["translate"];
 
-    std::cout << "table_bench: ";
+    std::cout << "cpp_glue_lua_all_aot: ";
     bench([&](){
         for (sol::table& v : vs) {
             translate(v);
@@ -147,12 +225,32 @@ int main( int argc, const char* argv[] ) {
         end
     )");
 
+    // Vec2 my_vec(1., 2.1);
+    // sol::object lua_ready = sol::make_object(lua, &my_vec);
+    //
+    // std::cout << my_vec.x << " " << my_vec.y << std::endl;
+    // lua["translate"](lua_ready);
+    // std::cout << my_vec.x << " " << my_vec.y << std::endl;
+    //
+    // return 0;
 
-    repeat(cpp_bench, 5);
-    repeat([&](){ ptr_bench(lua); }, 5);
-    std::cout << lua["sum"].get<float>() << std::endl;
-    repeat([&](){ table_bench(lua); }, 5);
-    std::cout << lua["sum"].get<float>() << std::endl;
+    repeat([&](){ native_bench(lua); }, 5);
+    std::cout << std::endl;
+
+    repeat([&](){ cpp_func_lua_mem(lua); }, 5);
+    std::cout << std::endl;
+
+    repeat([&](){ cpp_mem_lua_func(lua); }, 5);
+    std::cout << std::endl;
+
+    repeat([&](){ cpp_mem_lua_func_aot(lua); }, 5);
+    std::cout << std::endl;
+
+    repeat([&](){ cpp_glue_lua_all(lua); }, 5);
+    std::cout << std::endl;
+
+    repeat([&](){ cpp_glue_lua_all_aot(lua); }, 5);
+    std::cout << std::endl;
 
     return 0;
 }
